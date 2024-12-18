6e88a2dddc9c1a361b15aa17ae1defd1b4a82548
Altovskiy Maxim <altovskiy2006@gmail.com>
2024-12-18 15:54:41 +0200

    седьмая

---
+++ SeventhLaboratorka/func.hpp
@@ -0,0 +1,8 @@
+#pragma once
+int** createSquareMatrix(int n);
+int findSumOfArrayElem(int* array, int length, bool sumByAbs = false);
+void printSquareMatrix(int** matrix, int n);
+void fillSquareMatrix(int** matrix, int n);
+bool SquareMatrixHasIdenticalColumns(int** matrix, int n);
+bool SquareMatrixHasPrimeNumber(int** matrix, int n, bool useAbs = false);
+bool isPrime(int n);
